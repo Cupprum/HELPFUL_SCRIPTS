62709fa5665cb557c3c193326d8fdb22ccab8e86
Samuel Branisa <atrumoram@Samuels-MacBook-Pro.local>
2018-09-07 00:34:28 +0200

    complete database in final.txt

--- integrator.py
+++ integrator.py
@@ -9,4 +9,13 @@ class sign:
         wtw = f"{self.category}, {self.id}, {self.name}, {self.url}"
 
 
-txt_url = open("final_nurl.txt", "r")
+txt_url = open("final_url.txt", "r").readlines()
+txt_nonurl = open("final_nonurl.txt", "r").readlines()
+
+txt_w = open("final.txt", "w")
+
+for x in range(len(txt_url)):
+    p_first = txt_nonurl[x][:-1]
+    p_second = txt_url[x]
+
+    txt_w.write(f"{p_first}|{p_second}")
